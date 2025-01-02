1623ba7289659d86d53d25303c9c487742a3e479
Aprajita Pandey <142713632+aprajitapandeyxcghd@users.noreply.github.com>
2025-01-02 22:34:19 +0530

    implemented whether a stack is empty or not using java

---
+++ Data Structures/Stacks/emptyStack.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class emptyStack{
+  public static void main(String[] args){
+    Stack <String> GryffindorStack = new Stack<>();
+    if(GryffindorStack.isEmpty()){
+      System.out.println("Gryffindor Stack is emopty");
+    }
+    else{
+      System.out.println("Gryffindor Stack is not empty");
+    }
