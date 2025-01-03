666795d80c23d7f291d05ed008a9200b1100ccc2
Aprajita Pandey <142713632+aprajitapandeyxcghd@users.noreply.github.com>
2025-01-03 21:46:22 +0530

    coded and implemented a solid triangle pattern using java

---
+++ Problem Solving/Patterns/solidTriangle.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class solidTriangle{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter rows: ");
+    int rows = sc.nextInt();
+    System.out.println("Enter columns: ");
+    int cols = sc.nextInt();
+
+   for(int i=1; i<=rows;i++){
+     for(int j=1; j<=cols;j++){
+       System.out.print("*");
+     }
+       System.out.println()
+         }
+    sc.close();
+  }
+}
+
+       
