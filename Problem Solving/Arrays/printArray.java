ecb5ff0d7b7195506348f80c625a4702b7dc5758
Aprajita Pandey <142713632+aprajitapandeyxcghd@users.noreply.github.com>
2025-01-06 23:00:57 +0530

    printed a set of given input arrays using java

---
+++ Problem Solving/Arrays/printArray.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public clas printArray{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int[] array = {10, 20, 30, 40, 50, 60};
+    System.out.println("Array Elements");
+    for(int num : array){
+      System.out.print( num + " ");
+    }
+      System.out.println();
+  }
+}
