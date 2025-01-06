f859c694d20adf8cb1e706eea47da025a425e839
Aprajita Pandey <142713632+aprajitapandeyxcghd@users.noreply.github.com>
2025-01-06 23:09:19 +0530

    coded and implemented the printing of arrays through user input in java

---
+++ Problem Solving/Arrays/userInput.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class userInput{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter size of the array: ");
+    int size = sc.nextInt();
+
+    int[] array = new int[size];
+
+    System.out.println("Enter" + size + "elements");
+    for(int i=0; i<=size; i++){
+      System.out.println("Element" + (i+1) + " : ");
+      array[i] = sc.nextInt();
+    }
+
+    System.out.println("You entered the following array: ");
+    for(int num : array ){
+      System.out.println(num + " ");
+    }
+  }
+}
